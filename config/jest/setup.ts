6a7a3b05454364f7807633385f0bb8667e8e63df
Daniel Perez Alvarez <unindented@gmail.com>
2020-04-07 02:15:16 -0700

    test: fail tests on unhandled rejection

--- config/jest/setup.ts
+++ config/jest/setup.ts
@@ -1,3 +1,10 @@
+expect.assertions(1);
+
+process.on("unhandledRejection", (err) => {
+  // eslint-disable-next-line @typescript-eslint/no-throw-literal
+  throw err;
+});
+
 jest.mock("supports-color", () => ({
   supportsColor: (): boolean => true,
   stdout: { level: 3, hasBasic: true, has256: true, has16m: true },
